9a3a7b3e3f07cfb254d721290ad8886caa8cf228
Jonas Posselt <jonas.posselt@web.de>
2022-03-02 22:15:33 +0100

    actually implement TFModel

--- java/code/src/behavior/TFModel.java
+++ java/code/src/behavior/TFModel.java
@@ -1,40 +1,42 @@
 package behavior;
 
 import org.tensorflow.*;
+import tools.Point;
 
-public class TFModel {
+public class TFModel implements IBehavior<Point, Integer> {
+    private final Session session;
 
-    public static void main(String[] args) {
-        final String modelDir = "D:\\Repos\\DungeonRL\\py\\code\\model-tf\\agent-200";
+    public TFModel(String modelDir) {
         final String modelTag = "serve";
+        this.session = SavedModelBundle.load(modelDir, modelTag).session();
+    }
+
+    @Override
+    public Integer nextAction(Point state) throws IllegalArgumentException {
         final String inputOp  = "serving_default_args_0";
         final String maskOp   = "serving_default_mask";
         final String detOp    = "serving_default_deterministic";
         final String outputOp = "StatefulPartitionedCall";
 
-        SavedModelBundle b = SavedModelBundle.load(modelDir, modelTag);
-        Session s = b.session();
-
-        Tensor<Float> input           = Tensor.create(new float[][] {{2.0f, 2.0f}}, Float.class);
+        Tensor<Float> input           = Tensor.create(new float[][] {{state.x, state.y}}, Float.class);
         Tensor<Boolean> mask          = Tensor.create(new boolean[][] {{true, true, true, true}}, Boolean.class);
         Tensor<Boolean> deterministic = Tensor.create(true, Boolean.class);
 
-        Tensor<?> result = s.runner()
+        Tensor<?> result = this.session.runner()
                 .feed(inputOp, input)
                 .feed(maskOp, mask)
                 .feed(detOp, deterministic)
                 .fetch(outputOp)
                 .run().get(0);
 
-        long action = result.copyTo(new long[1])[0];
+        Integer action = (int) result.copyTo(new long[1])[0];
 
-        result.close();
         deterministic.close();
-        mask.close();
+        result.close();
         input.close();
+        mask.close();
 
-        s.close();
-        b.close();
+        return action;
     }
 
 }
